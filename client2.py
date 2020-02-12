10fa5ad50cdeefa6464e4cea9cce99e62c9f6fa9
iklob <henrique.cadastro@live.com>
2020-02-11 22:37:20 -0300

    Add files via upload

---
+++ client2.py
@@ -0,0 +1,21 @@
+import socket
+client_socket = socket.socket()
+port = 12345
+client_socket.connect(('127.0.0.1',port))
+
+recv_msg = client_socket.recv(1024)
+print recv_msg
+
+send_msg = raw_input("Enter your user name(prefix with #):")
+client_socket.send(send_msg)
+
+while True:
+    recv_msg = client_socket.recv(1024)
+    print recv_msg
+    # send_msg = raw_input("Send your message in format [@user:message] ")
+    send_msg = raw_input("Message: ")
+    if send_msg == 'exit':
+        break;
+    else:
+        client_socket.send(send_msg)
+client_socket.close()
